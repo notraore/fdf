db2f318cdaf75dd886d83a4bc5f7530656f13271
Nobila <nobila@Nobilas-Mac.local>
2017-07-05 02:39:58 +0200

    PushingWork

--- fdf.h
+++ fdf.h
@@ -2,20 +2,50 @@
 # define FDF_H
 
 #include "./libft/libft.h"
+# include <unistd.h>
+# include <fcntl.h>
+# define WIDTH 600
+# define HEIGHT 600
 
+/*
+**Structure des varialbes de la minilibX;
+*/
 typedef struct		s_tool
 {
-	int		x;
-	int		y;
-	int		sl;
-	int		color;
-	int		bpp;
-	int		end;
-	int		*img_data;
-	void	*mlx;
-	void	*win;
-	void	*img;
+	int				x;
+	int				y;
+	int				sl;
+	int				color;
+	int				bpp;
+	int				end;
+	int				*img_data;
+	void			*mlx;
+	void			*win;
+	void			*img;
 }					t_tool;
+
+/*
+** Liste chainee contenant dans chaque maillons les lignes lues;
+*/
+typedef struct	s_lst
+{
+	char			**map;
+	struct	s_lst	*next;	
+}				t_lst;
+
+
+/*
+**	Variables utilises pour le parcing du fichier depuis lequel lire;
+*/
+typedef	struct		s_parce
+{
+	int				fd;
+	int				value;
+	ssize_t			i;
+	char			*line;
+}					t_parce;
+
+
 
 /*
 ** input.c
